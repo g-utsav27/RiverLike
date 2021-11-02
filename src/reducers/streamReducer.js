8fbea3ff748b03686656597a66c2859d135e8ccb
Utsav Gautam <85408382+g-utsav27@users.noreply.github.com>
2021-11-03 00:51:36 +0530

    Update streamReducer.js

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -10,13 +10,13 @@ import {
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAMS:
-            return { ...state, ..._.mapKeys(action.paylaod, 'id') };
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM:
-            return { ...state, [action.paylaod.id]: action.payload };
+            return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
             return _.omit(state, action.payload);
         default:
